b4c2d7f577a45754241c69141a2f0516e9ffd20c
Taylor Lipson <taylor.lipson@gmail.com>
2020-02-15 10:08:51 -0500

    fixed arg parse

--- AlgoTradingPlatform/AlgoTradingPlatform.py
+++ AlgoTradingPlatform/AlgoTradingPlatform.py
@@ -5,26 +5,37 @@ import argparse
 import numpy as np
 
 
-def parse_args(arg_list):
-    parser = argparse.ArgumentParser()
+def parsing_args(arg_list):
+    parser = argparse.ArgumentParser(description="Algo Platform to perform data analytics and modelling")
     parser.add_argument("-a", "--api_key", help="Sets the api key to connect to AlphaVantage")
     return parser.parse_args(arg_list)
 
 
 # My API Key = AGFVK8HO7O4A8022
 def main(args):
-    # args = parse_args(args)
+    args = parsing_args(args)
     ticker = "SPY"
-    grabber = DataGrabber(api_key="AGFVK8HO7O4A8022", cache_dir=r"/TickerDataCache")
-    data_frame = grabber.getTickerData(ticker, "5min", "2019-05-17 10:00:00", "2019-05-24 16:00:00")
+
+    ## Data Grabber
+                # api_key = "AGFVK8HO7O4A8022"
+    grabber = DataGrabber(api_key=args.api_key, cache_dir=r"C:\Repositories\TickerCache")
+    data_frame = grabber.getTickerData(ticker, "7day", "2019-02-11 8:00:00", "2020-02-14 16:00:00")
     print(data_frame)
-    rsi = RSI()
-    rsi.calculate(data_frame["Close"].values, 14)
-    data_frame["RSI"] = np.zeros(data_frame["Close"].shape[0])
-    data_frame["RSI"][1:] = rsi.value
+
+
+    ## Data Analyzing
+    # rsi = RSI()
+    # rsi.calculate(data_frame["Close"].values, 14)
+    # data_frame["RSI"] = np.zeros(data_frame["Close"].shape[0])
+    # data_frame["RSI"][1:] = rsi.value
+
+
+
+
+    # Data Viewing
     viewer = DataViewing()
-    print(data_frame["RSI"])
+    # print(data_frame["RSI"])
     # viewer.TimeValueChart(data_frame, ticker)
-    viewer.Plot(data_frame, "RSI")
-    viewer.TimeValueChart(data_frame, ticker)
+    # viewer.Plot(data_frame, "RSI")
+    # viewer.TimeValueChart(data_frame, ticker)
 
--- AlgoTradingPlatform/__main__.py
+++ AlgoTradingPlatform/__main__.py
@@ -2,4 +2,4 @@ from .AlgoTradingPlatform import main
 from sys import argv
 
 if __name__ == "__main__":
-    main(argv)
+    main(argv[1:])
